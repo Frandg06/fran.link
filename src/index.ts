0ac2ed063012827dd0e0cfd343d0005b68a7deba
frandg06 <fdiez86@gmail.com>
2025-04-25 12:13:28 +0200

    ✨ feat(cors): añadir middleware CORS para permitir solicitudes desde dominios específicos

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,17 @@ import { list } from './controllers/list';
 import { post } from './controllers/create';
 import { update } from './controllers/update';
 import { destroy } from './controllers/delete';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-
+app.use(
+  cors({
+    origin: ['http://localhost', 'http://127.0.0.1', 'https://frandg.link', 'https://www.frandg.link'],
+    allowMethods: ['POST', 'GET', 'OPTIONS', 'PATCH', 'DELETE'],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 app.get('/:hash', get);
 app.get('/api/link', authMiddleware, list);
 app.post('/api/link', authMiddleware, post);
